8d9857918dce9cd2a66758a05fd6e454ea832920
Sergio Cruz <sergioczp.00@gmail.com>
2023-06-11 14:26:30 -0600

    layout mobile done

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ btnMenu.addEventListener("click", function () {
     btnMenu.firstElementChild.src = "assets/images/icon-menu-close.svg";
   }
   btnMenu.firstElementChild.classList.toggle("btn-menu__icon-close");
-  document.querySelector("body").classList.toggle("body-shadow");
+  // document.querySelector("body").classList.toggle("body-shadow");
   document
     .getElementsByClassName("nav__container-wd-255")[0]
     .classList.toggle("nav__container-wd-255--active");
+  document.querySelector(".future-main").classList.toggle("future-bg-shadow");
+  document.querySelector(".header").classList.toggle("header-shadow");
+  document.querySelector(".news").classList.toggle("news-bg-color");
 });
